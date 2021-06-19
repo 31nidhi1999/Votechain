ce3619af8ef567350bff6b286f3d0b8cf09a4eeb
31nidhi1999 <49243280+31nidhi1999@users.noreply.github.com>
2021-06-19 13:59:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 require('babel-core/register');
 require('babel-polyfill');
 require('./app');
+const msRest = require("@azure/ms-rest-js");
+const Face = require("@azure/cognitiveservices-face");
+const uuid = require("uuid/v4");
